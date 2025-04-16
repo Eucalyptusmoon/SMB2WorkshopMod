e666f2465a883638bfdc5e3c680a87081172b995
Eucalyptusmoon <moonlightnightlight123@gmail.com>
2025-04-16 16:02:57 -0700

    perfect-bonus-completion now adds goal score

--- src/patches/tweaks/perfect_bonus.cpp
+++ src/patches/tweaks/perfect_bonus.cpp
@@ -10,7 +10,18 @@ namespace perfect_bonus {
 TICKABLE_DEFINITION((
         .name = "perfect-bonus-completion",
         .description = "Perfect bonus completion",
-        .init_main_loop = init_main_loop, ))
+        .init_main_loop = init_main_loop,
+        .init_main_game = init_main_game))
+
+static patch::Tramp<decltype(&mkb::smd_game_bonus_clear_init)> s_smd_game_bonus_clear_init_tramp;
+
+// We want to have the score you'd get for a blue goal to be added so
+// there isn't a tradeoff between not getting a banana and going for
+// the goal instead
+void add_goal_score() {
+    mkb::mode_info.g_some_timer_frame_remaining_count = mkb::mode_info.stage_time_frames_remaining;
+    mkb::increment_score(2, 0);
+}
 
 // If the stage is a bonus stage (ball mode 0x40) and no bananas remain,
 // end the stage with a perfect (|= 0x228)
@@ -26,6 +37,13 @@ void init_main_loop() {
                 mkb::mode_info.ball_mode |= 0x228;
             }
         });
+}
+
+void init_main_game() {
+    patch::hook_function(s_smd_game_bonus_clear_init_tramp, mkb::smd_game_bonus_clear_init, []() {
+        add_goal_score();
+        s_smd_game_bonus_clear_init_tramp.dest();
+    });
 }
 
 }// namespace perfect_bonus
--- src/patches/tweaks/perfect_bonus.h
+++ src/patches/tweaks/perfect_bonus.h
@@ -3,5 +3,6 @@
 namespace perfect_bonus {
 
 void init_main_loop();
+void init_main_game();
 
 }// namespace perfect_bonus
