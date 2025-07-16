d87779b0b2eab8ac191d75aa891aa54f4ca72cab
Eucalyptusmoon <moonlightnightlight123@gmail.com>
2025-07-16 15:45:24 -0700

    Fix Play Point sprites appearing on Continue screen

--- src/patches/removals/remove_playpoints.cpp
+++ src/patches/removals/remove_playpoints.cpp
@@ -25,6 +25,10 @@ void init_main_game() {
 
     // Removes playpoint screen when saving game data in story mode.
     patch::write_nop(reinterpret_cast<void*>(0x80274c94));
+
+    // Removes playpoint message on continue screen.
+    patch::write_nop(reinterpret_cast<void*>(0x808f653c));
+    patch::write_word(reinterpret_cast<void*>(0x803dcae0), 0x38600001);
 }
 
 void init_sel_ngc() {
@@ -34,5 +38,15 @@ void init_sel_ngc() {
 
 void tick() {
     mkb::unlock_info.party_games = 0x0001b600;
+    // We have to do this hacky stuff to get some Continue screen sprites to work properly
+    if (mkb::sub_mode == mkb::SMD_GAME_CONTINUE_INIT) {
+        mkb::g_playpoint_msg_counter = 0x1;
+    }
+    else if (mkb::sub_mode == mkb::SMD_GAME_RESTART_INIT || mkb::sub_mode == mkb::SMD_GAME_NAMEENTRY_READY_INIT || mkb::sub_mode == mkb::SMD_GAME_ROLL_INIT) {
+        mkb::g_playpoint_msg_counter = 0x0;
+    }
+    else {
+        return;
+    }
 }
 }// namespace remove_playpoints
